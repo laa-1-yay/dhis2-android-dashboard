201e1389c5a0b492780469ab910c4b807e93fdf5
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-03-05 14:55:02 +0530

    General Reusable Snackbar messages and Internet Status Utilities
    Signed-off-by: Laavanye Bahl <laavanye.bahl@gmail.com>

---
+++ app/src/main/java/org/hisp/dhis/android/dashboard/utils/Utilities.java
@@ -0,0 +1,45 @@
+package org.hisp.dhis.android.dashboard.utils;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by laavanye on 5/3/16.
+ */
+
+public class Utilities {
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static Boolean isOnline() {
+        try {
+            Process p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
+            int returnVal = p1.waitFor();
+            boolean reachable = (returnVal==0);
+            return reachable;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void showSnackbar(View view, String message){
+        Snackbar snackbar = Snackbar.make(view, message,
+                Snackbar.LENGTH_LONG);
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.WHITE);
+        snackbar.show();
+    }
+}
