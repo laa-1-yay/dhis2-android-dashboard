75f2296389d9f7791915fcb4a315a8659e463abf
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-06-09 15:02:45 +0530

    Basic ui and file structure

---
+++ app/src/main/java/org/hisp/dhis/android/dashboard/App.java
@@ -0,0 +1,28 @@
+package org.hisp.dhis.android.dashboard;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import org.hisp.dhis.client.sdk.ui.bindings.commons.Inject;
+import org.hisp.dhis.client.sdk.ui.bindings.commons.NavigationHandler;
+import org.hisp.dhis.client.sdk.ui.bindings.views.DefaultLoginActivity;
+
+public final class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Inject.init(this, App.class.getCanonicalName(), App.class.getCanonicalName());
+
+        NavigationHandler.loginActivity(DefaultLoginActivity.class);
+        NavigationHandler.homeActivity(HomeActivity.class);
+    }
+
+    @Override
+    protected void attachBaseContext(Context baseContext) {
+        super.attachBaseContext(baseContext);
+        MultiDex.install(this);
+    }
+}
